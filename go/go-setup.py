05756df7f646ed3a9ee47c72156c8f588e67aca7
Shubhendu Poothia <shubhendu.poothia@india.nec.com>
2018-10-11 12:22:19 +0530

    Update go-setup.py

--- go/go-setup.py
+++ go/go-setup.py
@@ -16,11 +16,12 @@ open(filename , 'wb').write(r.content)
 tar = tarfile.open(filename, "r:gz")
 tar.extractall()
 tar.close()
-print(os.getcwd())
+
 os.system("sudo mv go/ /usr/local/")
 
 fh=open("/etc/profile", "a")
 fh.write("\n\nexport PATH=$PATH:/usr/local/go/bin")
 fh.close()
 
-print("Setup complete, Please log off and login again to see the effects.")
+os.system("source /etc/profile")
+print("Setup complete.")
