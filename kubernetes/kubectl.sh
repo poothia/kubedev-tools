a9c60995ed000698dbe13c8dd5ddbca33247e2ef
Shubhendu Poothia <poothia.shubhendu@gmail.com>
2019-09-11 13:11:28 +0530

    Update kubectl.sh

--- kubernetes/kubectl.sh
+++ kubernetes/kubectl.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
 
+#Installing Docker
+sudo sh ./docker.sh
+
+#Install kubectl binary with curl on Linux
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+sudo chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+kubectl version
