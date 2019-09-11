4be5ae727173d218ff9aec28be8d8908e84e6e3e
Shubhendu Poothia <poothia.shubhendu@gmail.com>
2019-09-11 13:06:17 +0530

    Update minikube.sh

--- kubernetes/minikube.sh
+++ kubernetes/minikube.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
 
+#Installing Kubectl
+sudo sh ./kubectl.sh
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+
+sudo install minikube /usr/local/bin
