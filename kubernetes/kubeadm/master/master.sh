2e99f6eaa381a014f1409cedc91adba946a2c625
poothia <poothia.shubhendu@gmail.com>
2020-02-27 21:25:00 +0530

    fixing permissions

--- kubernetes/kubeadm/master/master.sh
+++ kubernetes/kubeadm/master/master.sh
@@ -1,3 +1,6 @@
+#Installing Docker
+sudo sh ./../../docker.sh
+
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
