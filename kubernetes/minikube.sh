ac96a508b2423d98ab7600b50e415dcd3f68d6bb
Shubhendu Poothia <poothia.shubhendu@gmail.com>
2022-06-03 12:53:16 +0530

    Update minikube.sh

--- kubernetes/minikube.sh
+++ kubernetes/minikube.sh
@@ -6,7 +6,7 @@ sudo sh ./kubectl.sh
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube
 
-apt install conntrack
+sudo apt install conntrack
 sudo install minikube /usr/local/bin
 
 rm -rf minikube
